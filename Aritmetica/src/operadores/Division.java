1e7484b78bc72bb18c7478b88548cfba2208cd79
da2my <da2my@icloud.com>
2020-10-20 20:31:44 +0200

    Actualizada clase con métodos dividir nuevos, uno de ellos es de Bea que tiene incluido el resto de la división. algo que yo no lo tenia

--- Aritmetica/src/operadores/Division.java
+++ Aritmetica/src/operadores/Division.java
@@ -20,5 +20,51 @@ public class Division {
 		} while (num1 != 0);
 		return aux;
 	}
+	
+//	public static double dividir(double num1, double num2) {
+//	int aux = 0;
+//	int cont = 0;
+//	double resultado = 0;
+//	do {
+//		for (double i = 0; i <= num2; i++) {
+//			cont = (int) i;
+//		}
+//		num1 -= cont;
+//		aux++;
+//	} while (num1 != 0);
+//	return aux;
+//}
+	
+//	public static void dividir(double numD, double numd) {
+//	int aux = 0;
+//	int resto;
+//	int cont1 = 0;
+//	int cont = 0;
+//	do {
+//		for (int i = 0; i <= numd; i++) {
+//			cont = i;
+//		}
+//		cont1 += cont;
+//		aux++;
+//	} while (cont1 != numD);
+//	resto = (int) (multiplicar(aux, numd) - numD);
+//	System.out.println("La division es: " + aux);
+//	System.out.println("El resto de la division es:" + resto);
+//}
+	
+//	public static void dividir(double num1, double num2) {// forma de division de Bea
+//
+//		int cociente = 0;
+//		int resto = (int) num2;// el resto lo cargo con el dividendo, este es el matiz
+//		if (num1 == 0) {
+//			System.out.println("no se puede");
+//		} else {
+//			while (resto >= num2) {
+//				resto = (int) (resto - num2);
+//				cociente = cociente + 1;
+//			}
+//		}
+//		System.out.println("dividendo: " + num1 + " divisor: " + num2 + " cociente: " + cociente + " resto: " + resto);
+//	}
 
 }
