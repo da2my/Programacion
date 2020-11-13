fdafd553f6685845d0679ea3bbae098c45168a9c
da2my <da2my.icloud.com>
2020-11-13 18:49:33 +0100

    actualizada proyecto Dinero, mejorado

--- Monedas/src/controller/Admin.java
+++ Monedas/src/controller/Admin.java
@@ -5,11 +5,9 @@ import model.Dinero;
 public class Admin {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Dinero b =new Dinero(95);
-//		b.moneda();
-		System.out.println("Dinero pedido: "+b.getCantidad());
-		b.moneda();
+		Dinero b = new Dinero(153.72);
+		System.out.println("Cantidad de dinero: " + b.getCantidad());
+		Dinero.moneda();		
 	}
 
 }
--- Monedas/src/model/Dinero.java
+++ Monedas/src/model/Dinero.java
@@ -1,8 +1,10 @@
 package model;
 
+import java.util.*;
+
 public class Dinero {
 
-	private double cantidad;
+	private static double cantidad;
 
 	public Dinero() {
 
@@ -11,38 +13,34 @@ public class Dinero {
 	public Dinero(double cantidad) {
 		this.cantidad = cantidad;
 	}
-	
 
 	public double getCantidad() {
 		return cantidad;
 	}
 
-	
-	
 	@Override
 	public String toString() {
 		return "Dinero [cantidad=" + cantidad + "]";
 	}
 
-	public void moneda() {
+	public static void moneda() {
+
 		double[] monedas = { 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01 };
 		double aux = 0;
 		double aux1 = 0;
 		double aux2 = 0;
-		System.out.print("Caja:	 ");
-		for (int j = 0; j < monedas.length; j++) {
-			System.out.print(monedas[j] + "€ || ");
-		}
-		System.out.println();
-		System.out.print("Cantidad:  ");
-		for (double m : monedas) {
+		for (int i = 0; i < monedas.length; i++) {
+			double auxX = monedas[i];
+			double m = monedas[i];
+			int posicion = i;
 			if (m > 0) {
-				aux = (int) (cantidad / m);//numero de cada billete, en este caso 2
+				aux = (int) (cantidad / m);// numero de cada billete, en este caso 2
 				aux1 = (aux * m);// el valor a restar de los billetes que quitamos, siempre enteros. 100
 				aux2 = cantidad - aux1;// el valor actual a seguir descomponiendo 35.72
 				cantidad = aux2;
+
 			}
-			System.out.print((int) aux + "	 |    ");
+			System.out.println("| " + (int) aux + " --> " + auxX + "€	|");
 		}
 	}
 }
