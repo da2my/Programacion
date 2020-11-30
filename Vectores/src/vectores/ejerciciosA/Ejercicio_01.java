b1244422e0cc1a5fe673ee7fdca02fc7859255f5
da2my <da2my.icloud.com>
2020-11-30 22:56:21 +0100

    Vectores. Ejercicio de uso de vectores y Ejercicio 1

---
+++ Vectores/src/vectores/ejerciciosA/Ejercicio_01.java
@@ -0,0 +1,19 @@
+package vectores.ejerciciosA;
+
+import java.util.Scanner;
+
+//1. Crea un programa que pida diez números reales por teclado, los almacene en un array, y
+//luego muestre todos sus valores.
+public class Ejercicio_01 {
+
+	public static void main(String[] args) {
+
+	}
+	
+	static void guardaArray() {
+		Scanner sc = new Scanner(System.in);
+		
+	}
+	
+
+}
