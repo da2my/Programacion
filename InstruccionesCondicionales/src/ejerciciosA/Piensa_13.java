5cc9edc19d969c791b6179214085e09b367f73d2
da2my <da2my.icloud.com>
2020-12-01 21:24:15 +0100

    se avanza delimitando los valores pero falta limar detalles

--- InstruccionesCondicionales/src/ejerciciosA/Piensa_13.java
+++ InstruccionesCondicionales/src/ejerciciosA/Piensa_13.java
@@ -11,24 +11,44 @@ public class Piensa_13 {
 	public static void adivina() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Piensa un numero!!");
-		double a = 50;
-		double aux = 1;
-		double cuarto = 25;
+		int a = 50;
+		double aux = 25;
+		int x = 0;
+		double aux1 = 75;
 		String posibilidad;
 		while (true) {
-			System.out.println("Este es tu numero " + (int) a + "?");// dentro de bucle while
+			System.out.println("Este es tu numero " + a + "?");// dentro de bucle while
 			posibilidad = sc.next();
 			if (posibilidad.equalsIgnoreCase("menor")) {
-				aux-=25;
-				a = (a + aux) / 2;
-				if (a == 0) {
-					a = 1;
+				if (a == 50 || a > 25 && a < 50) {// inicial
+					x = a + 25;
+					a = x / 2;
+				} else if (a > 1 && a <= 25) {
+					x = a + 1;
+					a = x / 2;
 				}
+				if (a > 50 && a < 75) {
+					x = a + (50);
+					a = x / 2;
+				} else if (a > 75 && a < 100) {
+					x = a + (75);
+					a = x / 2;
+				}
+				
 			} else if (posibilidad.equalsIgnoreCase("mayor")) {
-				aux+=25;
-				a = (aux + a) / 2;
-				if (a > 100) {
-					a = 100;
+				if (a == 50 || a > 50 && a < 75) {//inicial
+					x = a + (75);
+					a = x / 2;
+				} else if (a > 75 && a < 100) {
+					x = a + (100);
+					a = x / 2;
+				}
+				if (a > 25 && a < 50) {// inicial
+					x = a + (50);
+					a = x / 2;
+				} else if (a > 1 && a < 25) {
+					x = a + (25);
+					a = x / 2;
 				}
 			}
 		}
