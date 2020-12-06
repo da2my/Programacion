f2cc3a14de30b551134f8f2914552c603e1d4113
da2my <da2my.icloud.com>
2020-12-06 10:32:33 +0100

    Ejercicio_01 en dos metodos uno que guarda el array y otro que lo muestra

--- Vectores/src/vectores/ejerciciosA/Ejercicio_01.java
+++ Vectores/src/vectores/ejerciciosA/Ejercicio_01.java
@@ -7,13 +7,22 @@ import java.util.Scanner;
 public class Ejercicio_01 {
 
 	public static void main(String[] args) {
+		System.out.println("Introduce valores para mostrar: ");
+		muestraArray(guardaArray());
+	}
+
+	static double[] guardaArray() {
+		Scanner sc = new Scanner(System.in);
+		double arr[] = new double[10];
+		for (int i = 0; i < arr.length; i++)
+			arr[i] = sc.nextDouble();
+		return arr;
+	}
+
+	static void muestraArray(double[] arr) {
+		for (int i = 0; i < arr.length; i++)
+			System.out.println(arr[i]);
 
 	}
-	
-	static void guardaArray() {
-		Scanner sc = new Scanner(System.in);
-		
-	}
-	
 
 }
