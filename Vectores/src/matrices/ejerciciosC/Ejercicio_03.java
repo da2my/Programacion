ce7b75eccd2d7098aad533dd23fe84ea4957d374
david <david@da2my>
2021-01-13 20:35:46 +0100

    Matrices Ejercicio_03
    forma de introducir valores, por teclado, en cada campo de la matriz y
    contar cuantos valores son mayores, menores o iguales que 0

---
+++ Vectores/src/matrices/ejerciciosC/Ejercicio_03.java
@@ -0,0 +1,45 @@
+package matrices.ejerciciosC;
+
+import java.util.Scanner;
+
+public class Ejercicio_03 {
+
+	public static void main(String[] args) {
+		int max = 0;
+		int min = 0;
+		int igual = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Intorduce filas:");
+		int n = sc.nextInt();
+		System.out.println("Intorduce columnas:");
+		int m = sc.nextInt();
+		double[][] matriz = new double[n][m];
+		int filas = matriz.length;
+		int columnas = matriz[0].length;
+		for (int i = 0; i < filas; i++) {// Recorrido de las filas de la matriz
+			System.out.println();
+			for (int j = 0; j < columnas; j++) {// Recorrido de las celdas de una fila
+				// System.out.print("Matriz[" + i + "][" + j + "]=" + matriz[i][j]);
+				System.out.println("Introduce valor en la fila: " + i + "\n" + "Introduce valor en la columna: " + j);
+				matriz[i][j] = sc.nextInt();
+				if (matriz[i][j] > 0) {
+					max++;
+				} else if (matriz[i][j] < 0) {
+					min++;
+				} else {
+					igual++;
+				}
+			}
+		}
+
+		double[][] muestraMatriz = new double[n][m];
+		for (int i = 0; i < filas; i++) {// Recorrido de las filas de la matriz
+			System.out.println();
+			for (int j = 0; j < columnas; j++) {// Recorrido de las celdas de una fila
+				// System.out.print("Matriz[" + i + "][" + j + "]=" + matriz[i][j]);
+				System.out.print("|" + matriz[i][j] + "|");
+			}
+		}
+		System.out.println("\n"+"Valores menores de 0: " + min + ", Valores mayores a 0: " + max + ", Valores iguales a 0:" + igual);
+	}
+}
